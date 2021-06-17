9003c23a90785f543ea02ca47239494df3aedbb4
Shreesh Nayak <shreeshnayak@gmail.com>
2021-06-16 21:09:17 -0700

    Added fetching of data

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,28 @@ import Badge from '@material-ui/core/Badge';
 import AddShoppingCart from '@material-ui/icons/AddShoppingCart';
 // Styles
 import { Wrapper } from './App.styles';
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const fetchProducts = async (): Promise<CartItemType[]> => 
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', fetchProducts);
+
+  console.log('data', data);
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
   return (
     <div className="App">
       Start
